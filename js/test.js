a44afb7c64d2dabac85e8eb5dce42d8b0c797ab3
mekhti11 <mekhti11@gmail.com>
2017-03-08 14:25:40 +0300

    js added

---
+++ js/test.js
@@ -0,0 +1,16 @@
+function sayHello() {
+  console.log("Hello!");
+}
+
+function doSomething(thing){
+  console.log("doing the ",thing);
+}
+
+function sayingHello(name) {
+  console.log("Hello ",name);
+}
+
+
+sayHello();
+doSomething("own shit");
+sayingHello('R2-D2');
